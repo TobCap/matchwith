cf72b778ce479c9ab8b18025f8f347a25c0e0c8b
tob <tobakucapitalism@gmail.com>
2016-01-11 14:10:42 +0900

    Modify tests

--- tests/testthat/test-fizzbuzz.R
+++ tests/testthat/test-fizzbuzz.R
@@ -2,7 +2,7 @@ context("fizzbuzz test")
 
 test_that("fizzbuzz test", {
   fizzbuzz1 <- function(z) {
-    match_with(list(z %% 5, z %% 3)
+    match_with(list(z %% 3, z %% 5)
     , list(0, 0) -> "FizzBuzz"
     , list(0, .) -> "Fizz"
     , list(., 0) -> "Buzz"
@@ -18,10 +18,10 @@ test_that("fizzbuzz test", {
      , otherwise  -> as.character(z)
      )
   }
-  
-  ans20 <- c("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", 
+
+  ans20 <- c("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz",
           "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19", "Buzz")
-          
-  expect_identical(sapply(1:20, fizzbuzz1, ans20)
-  expect_identical(sapply(1:20, fizzbuzz2, ans20)
+
+  expect_identical(sapply(1:20, fizzbuzz1), ans20)
+  expect_identical(sapply(1:20, fizzbuzz2), ans20)
 })
