f13dad77c25542b885495b9c33ea2129b49d8090
tob <tobakucapitalism@gmail.com>
2016-01-11 14:11:46 +0900

    Remove unused functions

--- R/matchwith.R
+++ R/matchwith.R
@@ -81,95 +81,82 @@ NULL
 match_with <- (function() {
   wildcards <- c(quote(.), quote(`_`), quote(otherwise))
   wildcards_char <- lapply(wildcards, as.character)
+  bool_funs <- c("!", "==", "!=", "<", "<=", ">=", ">", "any", "all", "identical", "isTRUE")
 
-  equals_recursive <- function(c1, c2, wildcard = NULL, strict_int_dbl= FALSE) {
-    if (!is.null(wildcard) && !is.language(c2)) {
-      stop("when using a wildcard, c2 must be a call or a symbol") }
-
-    # coerce characters into symbols
-    if (!is.null(wildcard) && any(is.character(wildcard))) {
-      wildcard <- lapply(wildcard, as.symbol) }
-
-    # shortcut without a wildcard
-    if (is.null(wildcard) && strict_int_dbl) {
-      return(identical(c1, c2)) }
-    if (is.null(wildcard) && !strict_int_dbl && xor(is.numeric(c1), is.numeric(c2))) {
-      return(FALSE) }
-
-    # c2 may have wildcard symbol
-    out_fun <- function(c1, c2) {
-      ## When using `==`, symbols are coerced into character by
-      ## PRINTNAME(x) in C lang level (See next source code:
-      ## https://github.com/wch/r-source/blob/trunk/src/main/relop.c#L185 )
-      ## and comparison between a symbol and list of symbols may
-      ## mistake in a special situation: quote(`_`) == list(quote(`_`), "_").
-      ## The result in above is c(FALSE, TRUE).
-      c2_has_wildcard <-
-        !is.null(wildcard) && is.symbol(c2) &&
-        (as.character(c2) %in% wildcards_char)
-
-      if (c2_has_wildcard) TRUE
-      else if (length(c1) != length(c2)) FALSE
-      else if (length(c1) == 0) identical(c1, c2)
-      else if (length(c1) == 1) {
-        if (!strict_int_dbl && is.numeric(c1) && is.numeric(c2)) {
-          isTRUE(as.double(c1) == as.double(c2)) }
-        else {
-          identical(c1, c2) }}
-      else {
-        for(i in seq_along(c1)) {
-          if (!out_fun(c1[[i]], c2[[i]])) return(FALSE) }
-        return(TRUE) }
-    }
-    out_fun(c1, c2)
-  }
-
-  # extract_patterns
-
+   # extract_patterns
   check_matched <- function(expr_info, parent_frame, l_expr, r_expr) {
     l_expr_len <- length(l_expr)
     has_wildcard <- has_wc(l_expr)
     cons_pattern <- l_expr_len == 3 && l_expr[[1]] == "::"
+    all_vars_mod <- setdiff(all.vars(l_expr), wildcards_char)
 
-    if (l_expr_len == 1 && is.atomic(l_expr)) { # Constant Pattern
-      if (equals_recursive(l_expr, expr_info$value)) {
-        list(is_matched = TRUE, matched_value = eval(r_expr, parent_frame))
+    # return list(is_matched = , new_list = )
+    if (l_expr_len == 0) {
+      list(is_matched = identical(l_expr, expr_info$value), new_list = NULL)
+    } else if (l_expr_len == 1) {
+       # Constant Pattern, Wildcard Pattern, or kind of a guard clauses.
+      if (is.atomic(l_expr)) {
+        if (isTRUE(l_expr == expr_info$value)) {
+          list(is_matched = TRUE, new_list = NULL)
+        } else {
+          list(is_matched = FALSE, new_list = NULL)
+        }
+      } else if (is.symbol(l_expr)) { # Variable Pattern or Wildcard Pattern
+        if (has_wildcard) {
+          list(is_matched = TRUE, new_list = NULL)
+        } else {
+          list(is_matched = TRUE, new_list = setNames(list(expr_info$value), as.character(l_expr)))
+        }
       } else {
-        list(is_matched = FALSE, matched_value = quote(Nothing))
-      }
-    } else if (l_expr_len == 1 && is.symbol(l_expr)) { # Variable Pattern or Wildcard Pattern
-      if (has_wildcard) {
-        list(is_matched = TRUE, matched_value = eval(r_expr, parent_frame))
-     } else {
-        list(is_matched = TRUE, matched_value = eval(r_expr, setNames(list(expr_info$value), as.character(l_expr)), parent_frame))
+        # should be a call object of length one, i.e. x(), and it must return a boolean object.
+        if (isTRUE(eval(l_expr, parent_frame))) {
+          list(is_matched = TRUE, new_list = setNames(list(expr_info$value), as.character(l_expr)))
+        } else {
+          list(is_matched = FALSE, new_list = NULL)
+        }
       }
     } else if (cons_pattern) {
       if (is.symbol(l_expr[[2]]) && is.symbol(l_expr[[3]]))  {
-        list(is_matched = TRUE, matched_value = match_hdtl(expr_info$value, l_expr, r_expr, parent_frame))
+        list(is_matched = TRUE, new_list = match_hdtl(expr_info$value, l_expr, r_expr))
       } else {
-        stop("pattern of `x::xs` is only acceptable. `x::y::yss` is not supported")
+        stop("pattern of `x::xs` is only acceptable. `x::y::ys` is not supported")
       }
-    } else if (l_expr_len > 1 && # integer(0) shold be matched hear?
-               equals_recursive(expr_info$value_deparse(), l_expr, if (has_wildcard) wildcards else NULL)) {
-      # tuple pattern
-      list(is_matched = TRUE, matched_value = eval(r_expr, parent_frame))
-    } else if (isTRUE(eval(l_expr, parent_frame))) {
-      # Guard for test
-      list(is_matched = TRUE, matched_value = eval(r_expr, parent_frame))
-    } else{
-      #print("last if")
-      list(is_matched = FALSE, matched_value = quote(Nothing))
+    } else if (any(as.character(l_expr[[1]]) %in% bool_funs)) {
+      if (isTRUE(eval(l_expr, parent_frame))) {
+        list(is_matched = TRUE, new_list = NULL)
+      } else {
+        list(is_matched = FALSE, new_list = NULL)
+      }
+    } else if ({.m <- match_var(l_expr, expr_info$value_deparse()); .m[[1]]}) {
+      list(is_matched = TRUE, new_list = .m[[2]])
+    } else {
+      list(is_matched = FALSE, new_list = NULL)
     }
   }
 
-  match_hdtl <- function(expr_value, l_expr, r_expr, env_) {
-    lst <- `names<-`(
+  match_hdtl <- function(expr_value, l_expr, r_expr) {
+    `names<-`(
       list(expr_value[[1]], expr_value[-1]),
       list(as.character(l_expr[[2]]), as.character(l_expr[[3]]))
     )
-    eval(r_expr, lst, env_)
   }
   # not exported
+
+  match_var <- function(l_expr, expr_orig, acc = list()) {
+    # returns list of a result of matched and pairs of symbol and value
+    if (is.symbol(l_expr))
+      if (has_wc(l_expr)) list(TRUE, acc)
+      else if (identical(l_expr, expr_orig)) list(TRUE, acc)
+      else list(TRUE, c(setNames(list(expr_orig), as.character(l_expr)), acc))
+    else if (length(l_expr) != length(expr_orig)) list(FALSE, NULL)
+    else if (length(l_expr) == 0) list(isTRUE(identical(l_expr, expr_orig)), acc) # for list(), NULL
+    else if (is.atomic(l_expr)) list(isTRUE(l_expr == expr_orig), acc) # absorbs difference of numeric and integer
+    else if (is.recursive(l_expr) && is.recursive(expr_orig)) {
+      hd <- match_var(l_expr[[1]], expr_orig[[1]], acc)
+      tl <- match_var(as.list(l_expr[-1]), as.list(expr_orig[-1]), acc)
+      list(hd[[1]] && tl[[1]], c(hd[[2]], tl[[2]])) }
+    else list(FALSE, NULL)
+  }
 
   has_wc <- function(call_) {
     any(all.vars(call_) %in% wildcards_char)
@@ -202,59 +189,8 @@ match_with <- (function() {
       ans_info <- check_matched(expr_info, parent_frame, l_expr, r_expr)
 
       if (ans_info$is_matched) {
-        return(ans_info$matched_value)
+        return(eval(r_expr, ans_info$new_list, parent_frame))
       }
-
-      # pattern_atomic <- is.atomic(l_expr)
-      # pattern_cons <- length(l_expr) == 3 && l_expr[[1]] == "::"
-      # pattern_tuple <- length(l_expr) > 1 && l_expr[[1]] == "list"
-      # pattern_wc <- length(l_expr) == 1 && any(wildcards_char %in% cond_vars)
-      # cond_vars <- if (pattern_atomic) "" else all.vars(l_expr)
-      # is_wildcard <- length(l_expr) == 1 && any(wildcards_char %in% cond_vars)
-      #
-      # if (is_wildcard && i != length(conds)) {
-      #   stop("wildcard must be last part of arguments") }
-      #
-      # has_wildcard_in_expr <- any(wildcards_char %in% cond_vars)
-      #
-      # symbol_is_referred <- is.symbol(expr) &&
-      #   (as.character(expr) %in% cond_vars) && !has_wildcard_in_expr
-      #
-      # name_is_referred <- !is.null(expr_name) &&
-      #   (expr_name %in% cond_vars) && !has_wildcard_in_expr
-      #
-      # eval_arg_2nd <-
-      #   if (pattern_cons) {
-      #     `names<-`(
-      #       list(expr_value[[1]], expr_value[-1]),
-      #       list(as.character(l_expr[[2]]), as.character(l_expr[[3]]))
-      #     ) }
-      #   else if (name_is_referred) {
-      #     `names<-`(list(expr_value), list(expr_name)) }
-      #   else if (!pattern_atomic && l_expr[[1]] == "list" && !has_wildcard_in_expr) {
-      #     # list(0, x) list(y, x)
-      #     NULL }
-      #   else {
-      #     NULL }
-      #
-      # ans <- eval(statement[[2]], envir = eval_arg_2nd, enclos = parent_frame)
-      #
-      # #
-      # if (is_wildcard || pattern_cons) {
-      #   return(ans) } # `wildcard`, x::xs
-      # else if ((symbol_is_referred || name_is_referred) && eval(l_expr, eval_arg_2nd, parent_frame)) {
-      #   return(ans) } # is.null(x), x %% 2 == 0
-      # else if (pattern_atomic && equals_recursive(expr_value, l_expr)) {
-      #   return(ans) } # 1, "", NULL
-      # else if (!pattern_atomic && !pattern_cons && !has_wildcard_in_expr && equals_recursive(expr_value, eval(l_expr, parent_frame))) {
-      #   return(ans) } # list(1,2), numeric(0)
-      # else if (!pattern_atomic && has_wildcard_in_expr && equals_recursive(expr_value_deparse, l_expr, wildcard = wildcards)) {
-      #   return(ans) } # list(1,.),
-      # else if (!pattern_atomic && l_expr[[1]] == "list")
-      #   # # list(x, y)
-      # else {
-      #   # not matched in this loop
-      # }
     }
     stop("The input is non-matched pattern. Need to write proper
             syntax or set default wildcard `.` at last.")
